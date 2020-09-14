780080517bb23b9f01c64c3fe139bbd2189793c3
paulnguyen <paul.nguyen@sjsu.edu>
2020-09-13 22:55:54 -0700

    update umlparser to add yuml generator sdk

--- umlparser/src/main/java/umlparser/Main.java
+++ umlparser/src/main/java/umlparser/Main.java
@@ -2,9 +2,17 @@
 
 package umlparser ;
 
-import java.io.Console;
-import java.io.File;
+import java.io.*;
 
+/*
+    Apache HTTP Client 3.0 (Legacy)
+
+    http://hc.apache.org/httpclient-legacy/index.html
+    https://hc.apache.org/httpclient-3.x/apidocs/
+*/
+
+import org.apache.commons.httpclient.*;
+import org.apache.commons.httpclient.methods.*;
 
 /**
  * Main Entry Point.
@@ -29,7 +37,7 @@ final class Main {
     private static void touch ( String dir, String filename ) {
         File file = new File( dir, filename ) ;
         if ( !file.exists() ) {
-            try { 
+            try {
                 file.createNewFile()  ;
             } catch ( Exception e ) {
                 System.out.println( e ) ;
@@ -38,14 +46,32 @@ final class Main {
     }
 
     /**
+     * [copyInputStreamToFile description]
+     * @param  inputStream [description]
+     * @param  filename    [description]
+     * @throws IOException [description]
+     */
+    private static void copyInputStreamToFile(InputStream inputStream, String filename)
+    throws IOException {
+
+        File file = new File( filename );
+        try (FileOutputStream outputStream = new FileOutputStream(file)) {
+            int read;
+            byte[] bytes = new byte[1024];
+            while ((read = inputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+        }
+
+    }
+
+
+    /**
      * Main App Entry Point.
-     * @param args No args expected.
+     * @param args base-dir test-case
      */
     public static void main(final String[] args) {
-        String basedir = "" ;
-        String testdir = "" ;
-        String testcase = "" ;
-        String outdir = "" ;
+
         if ( DEBUG ) {
             System.out.printf( "[DEBUG] UML Parser #Args: %d%n", args.length ) ;
             for (int i = 0; i < args.length; i++)  {
@@ -53,22 +79,41 @@ final class Main {
             }
         }
         if ( args.length == 2 ) {
-            basedir = args[0] ;
-            testcase = args[1] ;
-            testdir = basedir + File.separator + testcase ;
-            outdir = basedir + File.separator + "output" + File.separator + testcase ;
-            System.out.printf("basedir: %s%n", basedir) ;
-            System.out.printf("testdir: %s%n", testdir) ;
-            System.out.printf("outdir: %s%n", outdir) ;
-            if ( testcase.equals("sequence") ) {
-                touch(outdir, testcase+".seq") ;    // sequence source
-                touch(outdir, testcase+".jpg") ;    // sequence output
-            } else {
-                touch(outdir, testcase+".java") ;   // umlgraph source
-                touch(outdir, testcase+".png") ;    // umlgraph output
+
+            String basedir = args[0] ;
+            String testcase = args[1] ;
+            String testdir = basedir + File.separator + testcase ;
+            String outdir = basedir + File.separator + "output" + File.separator + testcase ;
+            if ( DEBUG ) {
+                System.out.printf("basedir: %s%n", basedir) ;
+                System.out.printf("testdir: %s%n", testdir) ;
+                System.out.printf("outdir: %s%n", outdir) ;                
             }
+
+            /* 
+            ** Replace This Code with your work! 
+            */
+            if ( testcase.equals("sequence") ) {
+                touch(outdir, testcase + ".seq") ;  // sequence source
+                touch(outdir, testcase + ".jpg") ;  // sequence output
+            } else {
+
+                /* Sample usage of YUML Object */
+                if ( testcase.equals("test1") ) {
+                    String dsl_test1 = "[A|-x:int;-y:int(*)]1-0..*[B],\n" +
+                           "[A]-1[C],\n" +
+                           "[A]-*[D]\n" ;                    
+                    YUML uml = new YUML( basedir ) ;
+                    String image = uml.generate( dsl_test1, "test1" ) ;
+                    System.out.println("Generated: " + image) ;
+                } else {
+                    touch(outdir, testcase + ".yuml") ; // yuml source
+                    touch(outdir, testcase + ".jpg") ;  // yuml output
+                }
+            }
+
         } else {
-             System.out.println( "java -cp [jar file].jar umlparser.Main [base directory] [test case]" ) ;
+            System.out.println( "java -cp [jar file].jar umlparser.Main [base directory] [test case]" ) ;
         }
 
 
---
+++ umlparser/src/main/java/umlparser/YUML.java
@@ -0,0 +1,124 @@
+/* (c) Copyright 2020 Paul Nguyen. All Rights Reserved */
+
+package umlparser ;
+
+import java.io.*;
+import java.nio.file.*;
+
+/*
+    Apache HTTP Client 3.0 (Legacy)
+
+    http://hc.apache.org/httpclient-legacy/index.html
+    https://hc.apache.org/httpclient-3.x/apidocs/
+*/
+
+import org.apache.commons.httpclient.*;
+import org.apache.commons.httpclient.methods.*;
+
+/**
+ *  yUML Diagram Generator
+ */
+class YUML {
+
+	private boolean DEBUG = true ;
+	private String basedir = "" ;
+
+	public YUML( String basedir ) {
+		this.basedir = basedir ;
+	}
+
+	public String generate( String dsl, String testcase ) {
+
+		String testdir = "" ;
+		String outdir = "" ;
+
+		/* Setup Test and Output Directories */
+		testdir = basedir + File.separator + testcase ;
+		outdir = basedir + File.separator + "output" + File.separator + testcase ;
+
+		if (DEBUG) {
+			System.out.printf("basedir: %s%n", basedir) ;
+			System.out.printf("testdir: %s%n", testdir) ;
+			System.out.printf("outdir: %s%n", outdir) ;
+		}
+
+		try {
+
+			String uml_image = "" ;
+			String image_id = "" ;
+			HttpClient httpclient = new HttpClient();
+
+			/* 	POST DSL Text to Generate Diagram
+				Result Expected back is Image ID
+				of Diagram to Download from yUML Servers.
+			*/
+			PostMethod post = new PostMethod("https://yuml.me/diagram/scruffy/class");
+			NameValuePair[] data = {
+				new NameValuePair("dsl_text", dsl)
+			};
+			post.setRequestBody(data);
+			try {
+				httpclient.executeMethod(post);
+				if (DEBUG)
+					System.out.println("yUML POST STATUS: " + post.getStatusLine());
+				InputStream uid = post.getResponseBodyAsStream();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(uid));
+				String line;
+				StringBuffer buffer = new StringBuffer();
+				while ((line = reader.readLine()) != null) {
+					buffer.append(line);
+					buffer.append("\n");
+				}
+				reader.close();
+				uml_image = buffer.toString();
+				image_id = uml_image.split("\\.")[0];
+				if (DEBUG)
+					System.out.println("yUML Image ID: " + image_id ) ;
+			} finally {
+				post.releaseConnection();
+			}
+
+			/* 	Download Generated Diagram
+				in JPG Image Format.
+			*/
+			GetMethod httpget = new GetMethod("https://yuml.me/" + image_id + ".jpg");
+			try {
+				httpclient.executeMethod(httpget);
+				if (DEBUG)
+					System.out.println("yUML GET STATUS: " + httpget.getStatusLine());
+				InputStream in = httpget.getResponseBodyAsStream();
+				copyInputStreamToFile( in, outdir + File.separator + testcase + ".jpg") ;
+			} finally {
+				httpget.releaseConnection();
+			}
+
+			/* Save DSL Code */
+			File file = new File( outdir, testcase + ".yuml" ) ;
+			if ( file.exists() )
+				file.delete() ;
+			try {
+				String path = outdir + File.separator + testcase + ".yuml" ;
+				Files.write( Paths.get(path), dsl.getBytes());
+			} catch ( Exception e ) {
+				System.out.println( e ) ;
+			}
+
+		} catch (Exception e) {
+			System.err.println( e ) ;
+		}
+
+		return outdir + File.separator + testcase + ".jpg" ;
+	}
+
+	private void copyInputStreamToFile(InputStream inputStream, String filename) throws IOException {
+		File file = new File(filename) ;
+		try (FileOutputStream outputStream = new FileOutputStream(file)) {
+			int read;
+			byte[] bytes = new byte[1024];
+			while ((read = inputStream.read(bytes)) != -1) {
+				outputStream.write(bytes, 0, read);
+			}
+		}
+	}
+
+}
