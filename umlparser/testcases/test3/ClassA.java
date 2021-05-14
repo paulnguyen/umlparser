f7035c24292145e775e7e21f4b8f3ed43f7eb001
paulnguyen <paul.nguyen@sjsu.edu>
2021-05-14 16:42:32 -0700

    Update ClassA.java

--- umlparser/testcases/test3/ClassA.java
+++ umlparser/testcases/test3/ClassA.java
@@ -3,8 +3,7 @@ class ClassA {
     
 	private String message ; // public attribute via setters and getters
     private String bark ;
-    protected String foo ;
-    protected String bar ;
+
     String test ; // package scope
     
 	public String getMessage() { 
